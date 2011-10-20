be54b6ddb6edf6bfaa034ca25fa501d3ece4138c
justin barton <bartonjustin@gmail.com>
2011-10-20 23:48:58 +1300

    hello world sample project

---
+++ samples/hello/project.clj
@@ -0,0 +1,4 @@
+(defproject hello "1.0.0-SNAPSHOT"
+  :description "helloworld clojurescript"
+  :dev-dependencies [[lein-clojurescript "1.0.0-SNAPSHOT"]]
+)
---
+++ samples/hello/src/hello.cljs
@@ -0,0 +1,4 @@
+(ns hello)
+
+(defn ^{:export greet} greet [n]
+      (str "Hello " n))
