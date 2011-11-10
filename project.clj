dfe4cb774ef0ceaf3342ba6cd0b6a071ace855ea
justin barton <bartonjustin@gmail.com>
2011-11-11 00:57:53 +1300

    bump version 1.0.1. deps now clojure-1.3.0. update readme.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject lein-clojurescript "1.0.0-SNAPSHOT"
+(defproject lein-clojurescript "1.0.1-SNAPSHOT"
   :description "leiningen plugin for clojurescript"
-  :dependencies [[org.clojars.bartonj/clojurescript "1.0.0-SNAPSHOT"]
+  :dependencies [[org.clojars.bartonj/clojurescript "1.0.1-SNAPSHOT"]
                  [com.google.javascript/closure-compiler "r1352"]
                  [org.clojars.bartonj/closure-lib "r790"]])
