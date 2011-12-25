07980dfd7f2f52385ec7bc5c3f60c5ede18c5bae
Robert Levy <r.p.levy@gmail.com>
2011-12-25 09:26:14 -0500

    restore generic groupId

--- samples/hello/project.clj
+++ samples/hello/project.clj
@@ -1,6 +1,6 @@
 (defproject hello "1.0.0-SNAPSHOT"
   :description "helloworld clojurescript"
-  :dev-dependencies [[rplevy/lein-clojurescript "1.0.2-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojurescript "1.0.2-SNAPSHOT"]]
   :cljs-output-to "resources/public/js/hello.js"
   :cljs-output-dir "resources/public/js/out"
   :cljs-optimizations :advanced)
