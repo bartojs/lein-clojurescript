23c4f0c13617066047d92267d6d3ee1021fdd337
Justin Barton <bartonjustin@gmail.com>
2012-01-24 10:17:56 +1300

    updated project to include clojure1.3 now that were using the official distro which doesnt specify it

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject lein-clojurescript "1.1.0"
   :description "leiningen plugin for clojurescript"
-  :dependencies [[org.clojure/clojurescript "0.0-927"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojurescript "0.0-927"]
                  [com.google.javascript/closure-compiler "r1352"]
-                 [org.clojars.bartonj/closure-lib "r790"]]
+                 ]
   :repositories {"sonatype-oss"
                  "http://oss.sonatype.org/content/groups/public/"})
