11a67351fe91257f5691e74ae0f0f3856ec3d345
Jeremy Hughes <jedahu@gmail.com>
2012-01-25 20:26:10 +1300

    use :cljs-{externs,libs,foreign-libs} options

--- src/leiningen/clojurescript.clj
+++ src/leiningen/clojurescript.clj
@@ -42,6 +42,9 @@ examples: lein clojurescript
   (let [outputfile (str (or (:name project) (:group project)) ".js")
         opts (apply merge {:output-to (or (:cljs-output-to project) outputfile)
                            :output-dir (or (:cljs-output-dir project) "out")
+                           :externs (:cljs-externs project)
+                           :libs (:cljs-libs project)
+                           :foreign-libs (:cljs-foreign-libs project)
                            :optimizations (:cljs-optimizations project)}
                     (map read-string (filter clojurescript-arg? args)))
         sourcedir (or (:clojurescript-src project) (:src-dir opts) "src")
